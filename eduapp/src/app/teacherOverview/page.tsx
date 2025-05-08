27c3884b1480222fbe6d42beaaa9b1d671867531
Kelly5764 <Kelly5764@saskpolytech.ca>
2025-05-08 08:40:37 -0600

    added teacher main landing page

---
+++ eduapp/src/app/teacherOverview/page.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+
+import React, { useState } from "react";
+import { Bell   } from "lucide-react"; // icon library or image
+import { VscAccount } from "react-icons/vsc";
+import { getStudentName } from "@/app/teacherBoard/api/route";
+
+
+export default function TeacherOverview() {
+    const [userName, setUserName] = useState("sample");
+
+
+    return (
+        <>
+            <div className="flex items-center justify-center">
+                <header className="w-full bg-violet-700 px-6 py-4 flex justify-between items-center ">
+                    <button className="bg-violet-800 border-1 text-white px-4 py-2 rounded hover:bg-blue-700">
+                        Log Out
+                    </button>
+                    <div className="flex items-center gap-4">
+                        <Bell className="w-6 h-6 text-gray-700 text-white cursor-pointer"/>
+                        <VscAccount className="w-6 h-6 text-gray-700 text-white cursor-pointer"/>
+                    </div>
+                </header>
+            </div>
+            <div className="flex items-center justify-center mt-10">
+                <h1 className="text-4xl font-bold text-center text-black  "> Welcome to BTC Assessments </h1>
+            </div>
+            <div className="flex items-center justify-center mt-10 " >
+                <form className=" p-8 rounded-xl shadow-md w-full max-w-sm bg-gray-100">
+                <label className="mb-1 text-sm font-medium text-black flex items-center justify-center"></label>
+                    <button
+                        type="submit"
+                        className="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700"
+                    >
+                        Student Assignment View
+                    </button>
+                </form>
+            </div>
+            <div className="flex items-center justify-center mt-10 " >
+                <form className=" p-8 rounded-xl shadow-md w-full max-w-sm bg-gray-100">
+                    <label className="mb-1 text-sm font-medium text-black flex items-center justify-center"></label>
+                    <button
+                        type="submit"
+                        className="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700"
+                    >
+                        Template Creation and Editing
+                    </button>
+                </form>
+            </div>
+            <div className="flex items-center justify-center mt-10 " >
+                <form className=" p-8 rounded-xl shadow-md w-full max-w-sm bg-gray-100">
+                    <label className="mb-1 text-sm font-medium text-black flex items-center justify-center"></label>
+                    <button
+                        type="submit"
+                        className="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700"
+                    >
+                        Database Editing
+                    </button>
+                </form>
+            </div>
+        </>
+    )
+};
