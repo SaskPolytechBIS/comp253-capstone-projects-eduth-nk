776dc32b3f71de2af2ecc7531e7eee3da46077b2
ReeseBohn <bohn6421@saskpolytech.ca>
2025-05-08 08:32:26 -0600

    Update page.tsx

--- eduapp/src/app/login/page.tsx
+++ eduapp/src/app/login/page.tsx
@@ -80,7 +80,9 @@ export default function LoginPage() {
                 </div>
                 <button
                     type="submit"
-                    className="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700">
+                    className="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700"
+                    onClick={handleSubmit}
+                    >
                     Log In
                 </button>
             </form>
