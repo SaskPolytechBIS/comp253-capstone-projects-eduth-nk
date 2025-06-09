62c55006ea6f71dcb1a4fba46d2864dc5cf2c078
Viki Fang <fang6685@saskpolytech.ca>
2025-06-08 23:31:16 -0600

    Update page.tsx

--- eduapp/src/app/studentBoard/page.tsx
+++ eduapp/src/app/studentBoard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { Bell   } from "lucide-react"; // icon library or image
 import { VscAccount } from "react-icons/vsc";
